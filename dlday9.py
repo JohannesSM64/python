70a73a4fecb53dc94a7316fdfb6efea4c2279a6a
JohannesSM64 <johannes@sm64.org>
2014-05-20 22:19:16 +0200

    dlday9 doc + post

--- dlday9.py
+++ dlday9.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# This can be used to download Day[9]'s Brood War commentary videos. To use it,
+# you need to have movgrab installed, and opening post from this thread saved as
+# day9post.txt: http://www.teamliquid.net/forum/brood-war/335184-day-bw-dailies
+
 import subprocess, re
 
 for i in re.findall('(http://blip.+)\[/url\]',
