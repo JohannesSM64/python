71d47e789c7df7ccb3ba00a58727c29bec02e535
JohannesSM64 <johannes@sm64.org>
2014-05-29 23:42:06 +0200

    added more

--- yatpy.py
+++ yatpy.py
@@ -28,3 +28,30 @@ def two_pairs(dice):
                 saved = i
 def three_alike(dice): return amtfunc(dice, 3)
 def four_alike(dice): return amtfunc(dice, 4)
+def small_straight(dice):
+    for i in range(1,6):
+        if dice.count(i) == 0:
+            return None
+    return 15
+def large_straight(dice):
+    for i in range(2,7):
+        if dice.count(i) == 0:
+            return None
+    return 20
+def full_house(dice):
+    two_of = None
+    three_of = None
+    for i in reversed(range(1,7)):
+        amt = dice.count(i)
+        if amt == 2:
+            two_of = i
+        elif amt == 3:
+            three_of = i
+        if two_of and three_of:
+            return (two_of*2) + (three_of*3)
+chance = sum
+def yatzy(dice):
+    for i in dice:
+        if i != dice[0]:
+            return None
+    return 50
